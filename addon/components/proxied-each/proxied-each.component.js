283fdb65586a959bf40ef9b65f8cd9341b97e83f
Chris Thoburn <runspired@gmail.com>
2015-05-20 11:02:46 -0500

    remove init from proxied each, necessary setup is done by the mixin

--- addon/components/proxied-each/proxied-each.component.js
+++ addon/components/proxied-each/proxied-each.component.js
@@ -5,14 +5,6 @@ export default Ember.CollectionView.extend(MagicArrayMixin, {
 
   keyForId: null,
 
-  tagName: '',
-
-  init: function() {
-    if (!this.get('content')) {
-      this.set('content', Ember.A());
-    }
-    this._updateProxy();
-    this._super.apply(this, arguments);
-  }
+  tagName: ''
 
 });
