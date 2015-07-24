c8af44f6565d40b99ff9605fe2b9a87dcbe625eb
Chris Thoburn <runspired@gmail.com>
2015-07-23 19:28:20 -0500

    properly install necessary css

--- addon/components/proxied-each.js
+++ addon/components/proxied-each.js
@@ -1,5 +1,6 @@
-import Ember from "ember";
-import MagicArrayMixin from "../mixins/magic-array";
+import Ember from 'ember';
+import MagicArrayMixin from '../mixins/magic-array';
+import layout from '../templates/components/proxied-each';
 
 export default Ember.Component.extend(MagicArrayMixin, {
   tagName: ''
