13074daf3c18c57a7a05760085b94e2d98ab2629
Chris Thoburn <runspired@gmail.com>
2015-07-24 08:21:04 -0500

    fix layout and style imports

--- addon/components/proxied-each.js
+++ addon/components/proxied-each.js
@@ -3,5 +3,6 @@ import MagicArrayMixin from '../mixins/magic-array';
 import layout from '../templates/components/proxied-each';
 
 export default Ember.Component.extend(MagicArrayMixin, {
+  layout: layout,
   tagName: ''
 });
---
+++ tests/unit/routes/components/proxied-each-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:components/proxied-each', 'Unit | Route | components/proxied each', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
