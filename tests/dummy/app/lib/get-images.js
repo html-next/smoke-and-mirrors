eb9ad5746b2086cb40d3419fd165d04338ee1102
Chris Thoburn <runspired@gmail.com>
2015-10-12 17:19:49 -0500

    fixes a few bugs, adds dynamic image retrieval methods

--- tests/dummy/app/lib/get-images.js
+++ tests/dummy/app/lib/get-images.js
@@ -15,8 +15,12 @@ var CATEGORIES = [
   'technics'
 ];
 
-function isGray() {
+function booleanToss() {
   return Math.round(Math.random());
+}
+
+function isGray() {
+  return booleanToss();
 }
 
 function getRandomNumber(min, max) {
@@ -26,16 +30,25 @@ function getRandomNumber(min, max) {
 function getWidth() {
   return getRandomNumber(1500, 3000);
 }
+
+
+function getDynamicHeight() {
+  return getRandomNumber(300, 600);
+}
+
+function getDynamicWidth(height, isPortrait) {
+  return Math.round(isPortrait ? height / 16 * 9 : height / 9 * 16);
+}
+
 function generateImageId(index) {
   return (new Date()).getTime() + '-' + index;
 }
 function getId() {
-  return getRandomNumber(1, 10);
+  return getRandomNumber(0, 10);
 }
 
 function getCategoryIndex() {
-  var max = CATEGORIES.length - 1;
-  return getRandomNumber(0, max);
+  return getRandomNumber(0, CATEGORIES.length - 1);
 }
 
 function generateImageSrc(index) {
@@ -66,16 +79,61 @@ function generateImageSrc(index) {
   };
 }
 
-export default function getImages(count) {
+function generateDynamicImageSrc(index) {
+  var parts = [];
+  var preview = [];
+  parts.push(URL_BASE);
+  preview.push(URL_BASE);
+  if (isGray()) {
+    parts.push('g');
+    preview.push('g');
+  }
+  var height = getDynamicHeight();
+  var isPortrait = booleanToss();
+  parts.push(height);
+  parts.push(getDynamicWidth(height, isPortrait));
+  var small = 100;
+  preview.push(small);
+  preview.push(getDynamicWidth(small, isPortrait));
+  var cat = CATEGORIES[getCategoryIndex()];
+  parts.push(cat);
+  preview.push(cat);
+  var id = getId();
+  parts.push(id);
+  preview.push(id);
+  return {
+    large: parts.join('/'),
+    small: preview.join('/'),
+    id: generateImageId(index)
+  };
+}
 
+function getImages(count) {
   count = count || DEFAULT_COUNT;
-
   var imageUrls = [];
-
 
   for (var i = 1; i <= count; i++) {
     imageUrls.push(generateImageSrc(i));
   }
 
   return imageUrls;
+}
+
+function getDynamicImages(count) {
+  count = count || DEFAULT_COUNT;
+  var imageUrls = [];
+
+  for (var i = 1; i <= count; i++) {
+    imageUrls.push(generateDynamicImageSrc(i));
+  }
+
+  return imageUrls;
+}
+
+export default getImages;
+
+
+export {
+  getImages,
+  getDynamicImages
 }
