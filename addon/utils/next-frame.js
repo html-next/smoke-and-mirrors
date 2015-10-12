a03098efa5a69695b1c1d1c4168414d2cd433d60
Chris Thoburn <runspired@gmail.com>
2015-10-12 16:47:16 -0500

    optimizes next-frame, previously function calls using this couldn't be optimized

--- addon/utils/next-frame.js
+++ addon/utils/next-frame.js
@@ -9,22 +9,20 @@ const {
  * `Ember.run.next`, more similar to `Ember.run.later(context, method, ...params, 1000 / 16)`,
  * but uses `requestAnimationFrame` if available.
  */
-export default function scheduleIntoNextAmimationFrame(context, method) {
+export default function scheduleIntoNextAnimationFrame(context, method) {
+  let args = new Array(arguments.length);
+  for(var i = 0; i < args.length; ++i) {
+    //i is always valid index in the arguments object
+    args[i] = arguments[i];
+  }
 
-  var restArgs = Array.prototype.slice.call(arguments, scheduleIntoNextAmimationFrame.length);
-
-  // we should have at least two params
-  assert("scheduleIntoNextAnimationFrame expects a minimum of a context and a function as arguments.", arguments.length >= 2);
-
-  // ensure method is a function
-  assert("The second param provided to scheduleIntoNextAnimationFrame must be a function.", typeof method === 'function');
+  let restArgs = args.slice(2);
 
   // make requestAnimationFrame support contexts and params
-  var frameCallback = function scheduledFrameTask(method, orgRestArgs, granularity) {
-    orgRestArgs.unshift(granularity);
+  var frameCallback = function scheduledFrameTask(method, orgRestArgs/*, granularity*/) {
+    //orgRestArgs.unshift(granularity);
     return method.apply(this, orgRestArgs);
   }.bind(context, method, restArgs);
 
   return window.requestAnimationFrame(frameCallback);
-
 }
