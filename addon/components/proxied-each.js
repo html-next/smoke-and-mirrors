e085028592596fc60250ade58e48510470ea2005
Chris Thoburn <runspired@gmail.com>
2015-10-22 13:58:48 -0500

    removes proxied-each

--- addon/components/proxied-each.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import MagicArrayMixin from '../mixins/magic-array';
-import layout from '../templates/components/proxied-each';
-
-export default Ember.Component.extend(MagicArrayMixin, {
-  layout: layout,
-  tagName: ''
-});
--- app/components/proxied-each.js
+++
@@ -1,3 +0,0 @@
-import Component from "smoke-and-mirrors/components/proxied-each";
-
-export default Component;
