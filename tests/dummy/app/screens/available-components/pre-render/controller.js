3db377c067e02cfe3e5970fe96cf83116ab62631
Chris Thoburn <runspired@gmail.com>
2015-10-22 18:42:57 -0500

    updated example

--- tests/dummy/app/screens/available-components/pre-render/controller.js
+++ tests/dummy/app/screens/available-components/pre-render/controller.js
@@ -15,24 +15,21 @@ export default Controller.extend({
   actions: {
     render() {
       this.setProperties({
-        renderIntoElement: null,
-        shouldRenderIntoElement: true
+        renderIntoElement: null
       });
     },
 
     renderInto1() {
       let element = jQuery('#renderIntoMe1').get(0);
       this.setProperties({
-        renderIntoElement: element,
-        shouldRenderIntoElement: true
+        renderIntoElement: element
       });
     },
 
     renderInto2() {
       let element = jQuery('#renderIntoMe2').get(0);
       this.setProperties({
-        renderIntoElement: element,
-        shouldRenderIntoElement: true
+        renderIntoElement: element
       });
     },
 
