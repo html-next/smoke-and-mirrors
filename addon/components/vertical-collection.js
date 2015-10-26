bd889479297b6240e277ca8ed71ed344835f79e5
Chris Thoburn <runspired@gmail.com>
2015-10-26 10:13:36 -0500

    cleans up code docs and settings

--- addon/components/vertical-collection.js
+++ addon/components/vertical-collection.js
@@ -7,20 +7,25 @@ const {
 } = Ember;
 
 export default Component.extend(OcclusionMixin, {
-
-
+  /**!
+   * Defaults to `vertical-collection`.
+   *
+   * If itemTagName is blank or null, the `vertical-collection` will [tag match](../addon/utils/get-tag-descendant.js)
+   * with the `vertical-item`.
+   */
+  tagName: 'vertical-collection',
   layout: layout,
 
   //–––––––––––––– Required Settings
 
   /**!
-   * This height is used to give the `OcclusionItem`s height prior to their content being rendered.
-   * This height is replaced with the actual rendered height once content is rendered for the first time.
+   * This height is used to give the `vertical-item`s height prior to
+   * it's content being rendered.
    *
-   * If your content will always have the height specified by `defaultHeight`, you can improve performance
-   * by specifying `alwaysUseDefaultHeight: true`.
+   * This height is replaced with the actual rendered height once content
+   * is rendered for the first time.
    */
-  defaultHeight: "75px",
+  defaultHeight: 75,
 
   /**!
    * Cached value used once default height is
@@ -35,29 +40,10 @@ export default Component.extend(OcclusionMixin, {
    * Set this if you need to dynamically change the height of the container
    * (useful for viewport resizing on mobile apps when the keyboard is open).
    *
-   * Changes to this property's value are observed and trigger new view boundary
-   * calculations.
+   * Changes to this property's value will trigger new viewport boundary
+   * calculations.  This works for height or width changes, it's value is
+   * never actually used.
    */
-  // TODO how to deal with this horizontally vs vertically
-  containerHeight: null,
-
-  /**!
-   * Defaults to `div`.
-   *
-   * If itemTagName is blank or null, the `vertical-collection` will [tag match](../addon/utils/get-tag-descendant.js)
-   * with the `vertical-item`.
-   */
-  tagName: 'vertical-collection',
-
-
-  //–––––––––––––– Performance Tuning
-
-  /**!
-   * If true, dynamic height calculations are skipped and
-   * `defaultHeight` is always used as the height of each
-   * `OccludedView`.
-   */
-  // TODO alwaysUseDefaultWidth
-  alwaysUseDefaultHeight: false
+  containerSize: null
 
 });
