c8c2047c1ac470fc0747388a604d737efcc95092
Chris Thoburn <runspired@gmail.com>
2015-10-29 13:36:59 -0500

    Release version 0.4.2

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -15,7 +15,7 @@ module.exports = function(environment) {
       EXTEND_PROTOTYPES : true
     },
 
-    VERSION: "0.4.1",
+    VERSION: "0.4.2",
 
     APP: {
       // Here you can pass flags/options to your application instance
