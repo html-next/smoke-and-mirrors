979056a394445b9c3c767e212fb022cb03917c9e
Chris Thoburn <runspired@gmail.com>
2015-11-04 13:21:38 -0600

    more tests!

---
+++ tests/integration/components/vertical-collection-test.js
@@ -0,0 +1,47 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+moduleForComponent('vertical-collection', 'Integration | Component | vertical collection', {
+  integration: true
+});
+
+test('The Collection Renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('items', Ember.A([Ember.Object.create({ text: 'b' })]));
+
+  // Template block usage:
+  this.render(hbs`
+  <div style="height: 500px; width: 500px;">
+    {{#vertical-collection content=items as |item|}}
+      {{item.text}}
+    {{/vertical-collection}}
+  </div>
+  `);
+
+  assert.equal(this.$().find('vertical-item').length, 1);
+});
+
+/*
+test("The Collection Reveals it's children when `renderAllInitially` is true.", function(assert) {
+  assert.expect(1);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('items', Ember.A([Ember.Object.create({ text: 'b' })]));
+
+  // Template block usage:
+  this.render(hbs`
+  <div style="height: 500px; width: 500px;">
+    {{#vertical-collection content=items renderAllInitially=true as |item|}}
+      {{item.text}}
+    {{/vertical-collection}}
+  </div>
+  `);
+
+  assert.equal(this.$().find('vertical-item').first().get(0).innerHTML, 'b');
+});
+*/
