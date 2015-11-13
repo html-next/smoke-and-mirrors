e9b3f2691710a625396fd0c6db0ecd2d629eb48d
Chris Thoburn <runspired@gmail.com>
2015-11-12 18:26:09 -0600

    updates to demo

--- tests/dummy/app/screens/examples/large-grid/route.js
+++ tests/dummy/app/screens/examples/large-grid/route.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import { getDynamicImages } from 'dummy/lib/get-images';
+import getNumbers from 'dummy/lib/get-numbers';
 
 const {
   Route
@@ -10,7 +10,12 @@ export default Route.extend({
   model() {
     let pages = [];
     for (let i = 0; i < 100; i++) {
-      pages.push(getDynamicImages(20))
+     let data = {
+          numbers: getNumbers(0, 20, i + '-'),
+          first: 0,
+          last: 20
+        };
+      pages.push(data)
     }
     return {
       pages: pages
