b55ffac65707875db40a6ecefcef5115d27469f6
Chris Thoburn <runspired@gmail.com>
2015-11-19 18:27:08 -0600

    bump ember deps

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+// jscs: disable
 /* jshint node: true */
+
 'use strict';
 
 module.exports = {
   name: 'smoke-and-mirrors',
 
-  included : function (app) {
+  included: function(app) {
     if (!/production/.test(app.env)) {
       console.info('Smoke-and-mirrors:: Including Vertical-Collection Debug CSS');
       app.import('./vendor/debug.css');
@@ -12,7 +14,7 @@ module.exports = {
   },
 
   isDevelopingAddon: function() {
-    return false;
+    return true;
   }
 
 };
