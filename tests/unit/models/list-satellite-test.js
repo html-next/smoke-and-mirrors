7438473ef9548e668a64470d0252b913f6bbccad
Kyle Mellander <kylemellander@gmail.com>
2015-11-30 09:41:05 -0800

    Add tests for next & prev in ListSatellite model

--- tests/unit/models/list-satellite-test.js
+++ tests/unit/models/list-satellite-test.js
@@ -7,13 +7,21 @@ let App = {};
 module('Unit | Model | ListSatellite', {
 
   beforeEach(assert) {
+
     App.planetADiv = document.createElement('div');
     document.body.appendChild(App.planetADiv);
     App.planetBDiv = document.createElement('div');
     document.body.appendChild(App.planetBDiv);
 
-    App.componentA = { element: App.planetADiv };
-    App.componentB = { element: App.planetBDiv };
+    App.componentA = {  element: App.planetADiv,
+                        next() {},
+                        prev() {},
+                        satellite: true };
+    App.componentB = {  element: App.planetBDiv,
+                        next() {},
+                        prev() {},
+                        satellite: true };
+
   },
 
   afterEach(assert) {
@@ -29,6 +37,7 @@ test('should build correctly', function(assert) {
   assert.expect(4);
 
   let testListSatellite = new ListSatellite(App.componentA, [App.componentA, App.componentB]);
+
   assert.equal(testListSatellite.component, App.componentA, "compenent set");
   assert.equal(testListSatellite.element, App.planetADiv, "element set");
   assert.equal(testListSatellite.radar, undefined, "radar initialized and not set");
@@ -38,9 +47,28 @@ test('should build correctly', function(assert) {
 
 test('next and prev return null when only one component', function(assert) {
 
+  assert.expect(2);
+
   let testListSatellite = new ListSatellite(App.componentA, [App.componentA]);
 
   assert.equal(testListSatellite.next(), null, "no next element");
   assert.equal(testListSatellite.prev(), null, "no prev element");
+
+});
+
+test('next and prev work as expected', function(assert) {
+
+  assert.expect(4);
+
+  App.componentA.next = function() { return App.componentB; };
+  App.componentB.prev = function() { return App.componentA; };
+  
+  let testListSatelliteA = new ListSatellite(App.componentA, [App.componentA, App.componentB]);
+  let testListSatelliteB = new ListSatellite(App.componentB, [App.componentA, App.componentB]);
+
+  assert.equal(testListSatelliteA.next(), true, "Component B is after Component A");
+  assert.equal(testListSatelliteB.next(), null, "Nothing is after Component B");
+  assert.equal(testListSatelliteA.prev(), null, "Nothing is before Component A");
+  assert.equal(testListSatelliteB.prev(), true, "Component A is before Component B");
 
 });
