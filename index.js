ce9f94224502a70317e83f6cf1ad677df0e335af
Chris Thoburn <cthoburn@linkedin.com>
2016-09-24 00:38:27 -0700

    cleanup the debug css notice

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 // jscs: disable
 /* jshint node: true */
-
 'use strict';
+
+var chalk = require('chalk');
 
 module.exports = {
   name: 'smoke-and-mirrors',
 
   included: function(app) {
     if (!/production/.test(app.env)) {
-      console.info('Smoke-and-mirrors:: Including Vertical-Collection Debug CSS');
+      console.info(
+        chalk.grey("\n===================================================================\n") +
+        chalk.cyan("\tSmoke-and-mirrors\n") +
+        chalk.grey("\t:: Including CSS for Visual Debugger\n") +
+        chalk.grey("\t:: (included in non production builds only)\n") +
+        chalk.grey("\t:: To use, set ") + chalk.yellow("{{vertical-collection debug=true}}") +
+        chalk.grey("\n===================================================================\n")
+      );
       app.import('./vendor/debug.css');
     }
   },
